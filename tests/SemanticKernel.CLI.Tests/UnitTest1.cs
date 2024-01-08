3b131e8263cda03cbd768da9875375e83a51b8ef
Tyler-R-Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2024-01-08 18:00:21 +0000

    added test projects

---
+++ tests/SemanticKernel.CLI.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace SemanticKernel.CLI.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
